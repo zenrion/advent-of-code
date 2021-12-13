1e01b8d71283152adfec5e76f1681f609f1c4fe9
Logan Baker <loganbakerdev@gmail.com>
2021-12-12 19:29:20 -0600

    removed unfinished day 8

--- 2021/08/main.ts
+++
@@ -1,7 +0,0 @@
-import fs from 'fs';
-
-const numberAndSegmentLengths = [0, 2, 0, 4, 0, 0, 3, 7, 0, 0]; // index is the number value is the number of segments to make that number.
-
-
-const data = fs.readFileSync(__dirname + '/input.txt', 'utf-8')
-.split('\r\n')
